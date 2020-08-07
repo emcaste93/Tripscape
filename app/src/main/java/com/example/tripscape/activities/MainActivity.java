6059fbf25b62d89860698fd2f4b05e1e627d1c12
Emilio Castellano <emcaste93@gmail.com>
2020-08-07 16:25:45 +0200

    Added icons and buttons for next/prev page

--- app/src/main/java/com/example/tripscape/activities/MainActivity.java
+++ app/src/main/java/com/example/tripscape/activities/MainActivity.java
@@ -1,11 +1,13 @@
-package com.example.tripscape;
+package com.example.tripscape.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.tripscape.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
