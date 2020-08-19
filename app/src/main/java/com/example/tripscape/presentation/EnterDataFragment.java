dd23a0ce097bdec8e194bffa05e2a3b5e860006f
Emilio Castellano <emcaste93@gmail.com>
2020-08-19 19:29:57 +0200

    View Enter-Data added Calendars layout

--- app/src/main/java/com/example/tripscape/presentation/EnterDataFragment.java
+++ app/src/main/java/com/example/tripscape/presentation/EnterDataFragment.java
@@ -1,22 +1,75 @@
 package com.example.tripscape.presentation;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.tripscape.R;
 
-public class EnterDataFragment extends Fragment {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class EnterDataFragment extends Fragment {
+    Button buttonStartDate, buttonEndDate;
+    Context context;
+    DatePickerDialog  startDate, endDate;
     @Override
-    public View onCreateView(LayoutInflater inflador, ViewGroup contenedor,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View vista = inflador.inflate(R.layout.enter_data,
-                contenedor, false);
+        View vista = inflater.inflate(R.layout.enter_data,
+                container, false);
+        context = container.getContext();
+        buttonStartDate = vista.findViewById(R.id.buttonStartDate);
+        buttonEndDate = vista.findViewById(R.id.buttonEndDate);
+
+        setCalendars();
+
+        buttonStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDate.show();
+            }
+        });
+
+        buttonEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endDate.show();
+            }
+        });
         return vista;
+    }
+
+    private void setCalendars(){
+        final Calendar startCalendar = Calendar.getInstance();
+        startDate = new DatePickerDialog(context, R.style.datepicker, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                buttonStartDate.setText(simpleDateFormat.format(newDate.getTime()));
+            }
+
+        }, startCalendar.get(Calendar.YEAR), startCalendar.get(Calendar.MONTH), startCalendar.get(Calendar.DAY_OF_MONTH));
+
+        final Calendar endCalendar = Calendar.getInstance();
+        endDate = new DatePickerDialog(context, R.style.datepicker, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                buttonEndDate.setText(simpleDateFormat.format(newDate.getTime()));
+            }
+
+        }, endCalendar.get(Calendar.YEAR), endCalendar.get(Calendar.MONTH), endCalendar.get(Calendar.DAY_OF_MONTH));
     }
 
     @Override
